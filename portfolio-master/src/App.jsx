d76e8ccfc7eb462dfa83548bf9abe9a44640fc6c
louie <louiemorland@gmail.com>
2026-08-21 13:37:39 +0100

    Fixed site analytics

--- portfolio-master/src/App.jsx
+++ portfolio-master/src/App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next"
 import Header from "./components/header.jsx";
 import Home from "./components/home.jsx";
 import Projects from "./components/Projects";
